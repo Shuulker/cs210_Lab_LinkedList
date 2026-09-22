26c5f9ccd5972cc78b0f3d0717428fe08bccdde6
Shuulker <JosephSparrowRoberts26@gmail.com>
2026-09-22 15:16:03 -0700

    Updated Data.h

--- Data.h
+++ Data.h
@@ -2,7 +2,21 @@
 // Created by Samuel on 9/17/26.
 //
 
-#ifndef UNTITLED1_DATA_H
-#define UNTITLED1_DATA_H
+#pragma once
+#include <ostream>
+#include <string>
 
-#endif //UNTITLED1_DATA_H
+class Data {
+public:
+    Data(int numID, const std::string& name)
+    : numID_(numID), name_(name) {}
+    bool operator==(const Data& other) const {
+        return numID_ == other.numID_;
+    }
+    friend std::ostream& operator<<(std::ostream& out, const Data& d) {
+        return out << d.numID_ << " " << d.name_;
+    }
+private:
+    int numID_;
+    std::string name_;
+};
